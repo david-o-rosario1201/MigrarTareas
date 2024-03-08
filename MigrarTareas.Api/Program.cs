7bb9fbc7649d389ef9c06ad0e7a4e12bf4bb8008
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-08 14:16:47 -0400

    Agregando el CORS

--- MigrarTareas.Api/Program.cs
+++ MigrarTareas.Api/Program.cs
@@ -22,6 +22,13 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
+app.UseCors(options =>
+{
+	options.AllowAnyHeader();
+	options.AllowAnyMethod();
+	options.AllowAnyOrigin();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
