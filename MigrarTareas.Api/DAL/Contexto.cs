d15019139ee7e90092107d02acf54d368889047a
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-08 09:15:02 -0400

    Creación e Inyección del Contexto.

---
+++ MigrarTareas.Api/DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MigrarTareas.Api.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+        
+    }
+}
