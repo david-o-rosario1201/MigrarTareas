4254d5eebab8981f31b09a60205b16243b619106
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-10 16:17:53 -0400

    Implementación del modelo Tickets en el Contexto.

--- MigrarTareas.Api/DAL/Contexto.cs
+++ MigrarTareas.Api/DAL/Contexto.cs
@@ -13,4 +13,5 @@ public class Contexto : DbContext
     public DbSet<Prioridades> Prioridades { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
     public DbSet<Sistemas> Sistemas { get; set; }
+    public DbSet<Tickets> Tickets { get; set; }
 }
