78ae13a1cfd601343e2081a0b6fb160008645805
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-10 03:39:38 -0400

    Remodelación del modelo Clientes, ahora tiene los detalles ClientesDetalleCelulares y ClientesDetallesTelefonos

---
+++ Library/Models/ClientesDetalleCelulares.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class ClientesDetalleCelulares
+{
+	[Key]
+	public int Id { get; set; }
+
+	public int ClienteId { get; set; }
+
+	public string Celular { get; set; }
+}
---
+++ Library/Models/ClientesDetalleTelefonos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class ClientesDetalleTelefonos
+{
+	[Key]
+	public int Id { get; set; }
+
+	public int ClienteId { get; set;}
+
+    public string Telefono { get; set; }
+}
