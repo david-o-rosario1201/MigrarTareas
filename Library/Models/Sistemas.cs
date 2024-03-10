ccea37906cd305b8977bdf2244db1b7b26b27a97
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-10 13:59:39 -0400

    Creación del modelo Clientes e implementación en el Contexto.

---
+++ Library/Models/Sistemas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class Sistemas
+{
+	[Key]
+	public int SistemaId { get; set; }
+
+	[Required(ErrorMessage = "Error, debe incluir un nombre")]
+	public string Nombre { get; set; }
+}
