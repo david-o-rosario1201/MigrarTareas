fafc67bbc53cb6908dd293327be1a526016d1d13
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-10 00:27:29 -0400

    Le agregué un putnto (.) al final de cada oración.

--- Library/Models/Clientes.cs
+++ Library/Models/Clientes.cs
@@ -12,28 +12,28 @@ public class Clientes
     [Key]
     public int ClienteId { get; set; }
 
-    [Required(ErrorMessage = "Debe ingresar un nombre")]
+    [Required(ErrorMessage = "Debe ingresar un nombre.")]
     public string Nombre { get; set; }
 
-    [Phone(ErrorMessage = "Error, el formato del número de teléfono no es válido")]
-    [StringLength(10, MinimumLength = 10, ErrorMessage = "El número de teléfono debe tener exactamente 10 caracteres")]
-    [Required(ErrorMessage = "Error, debe ingresar un número de teléfono")]
+    [Phone(ErrorMessage = "Error, el formato del número de teléfono no es válido.")]
+    [StringLength(10, MinimumLength = 10, ErrorMessage = "El número de teléfono debe tener exactamente 10 caracteres.")]
+    [Required(ErrorMessage = "Error, debe ingresar un número de teléfono.")]
     public string Telefono { get; set; }
 
-    [Phone(ErrorMessage = "Error, el formato del número de celular no es válido")]
-    [StringLength(11, MinimumLength = 11, ErrorMessage = "El número de celular debe tener exactamente 10 caracteres")]
-    [Required(ErrorMessage = "Error, debe ingresar un número de celular")]
+    [Phone(ErrorMessage = "Error, el formato del número de celular no es válido.")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "El número de celular debe tener exactamente 10 caracteres.")]
+    [Required(ErrorMessage = "Error, debe ingresar un número de celular.")]
     public string Celular { get; set; }
 
-    [Required(ErrorMessage = "Error, debe ingresar un RNC")]
-    [StringLength(11, MinimumLength = 11, ErrorMessage = "El RNC debe tener exactamente 11 caracteres")]
+    [Required(ErrorMessage = "Error, debe ingresar un RNC.")]
+    [StringLength(11, MinimumLength = 11, ErrorMessage = "El RNC debe tener exactamente 11 caracteres.")]
     public string RNC { get; set; }
 
 
-    [EmailAddress(ErrorMessage = "Error, el formato del email no es válido")]
-    [Required(ErrorMessage = "Error, debe un ingresar un email")]
+    [EmailAddress(ErrorMessage = "Error, el formato del email no es válido.")]
+    [Required(ErrorMessage = "Error, debe un ingresar un email.")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "Debe ingresar una dirección")]
+    [Required(ErrorMessage = "Debe ingresar una dirección.")]
     public string Direccion { get; set; }
 }
