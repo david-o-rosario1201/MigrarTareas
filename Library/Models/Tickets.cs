ca3ea562b533ee3ea7164f64d06c873dda5234d1
david-o-rosario1201 <finnnn023@gmail.com>
2024-03-10 16:17:13 -0400

    Creación del modelo Tickets con su detalle.

---
+++ Library/Models/Tickets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class Tickets
+{
+	[Key]
+	public int TicketId { get; set; }
+
+	[Required(ErrorMessage = "Error, la fecha es requerida")]
+	[DataType(DataType.Date)]
+	public DateTime Fecha { get; set; } = DateTime.Today;
+
+	[ForeignKey("Clientes")]
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public int ClienteId { get; set; }
+
+	[ForeignKey("Sistemas")]
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public int SistemaId { get; set; }
+
+	[ForeignKey("Prioridades")]
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public int PrioridadId { get; set; }
+
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public string SolicitadoPor { get; set; }
+
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public string Asunto { get; set; }
+
+	[Required(ErrorMessage = "Error, debe llenar este campo")]
+	public string Descripcion { get; set; }
+
+
+
+	[ForeignKey("TicketId")]
+	public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
+}
---
+++ Library/Models/TicketsDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class TicketsDetalle
+{
+	[Key]
+	public int Id { get; set; }
+
+	public int TicketId { get; set; }
+
+	public string Emisor { get; set; }
+
+	public string Mensaje { get; set; }
+}
